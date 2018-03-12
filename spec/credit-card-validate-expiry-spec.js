703d45aad8e641eb080b9ccbc34ed97b5c1d4518
Thodoris Bais <thodoris.bais@gmail.com>
2018-03-12 21:49:40 +0100

    Created a more specific function 'isExpiryInvalid' for the module

--- spec/credit-card-validate-expiry-spec.js
+++ spec/credit-card-validate-expiry-spec.js
@@ -1,33 +1,33 @@
-var validateExpiry = require('../index');
+var creditCardValidateExpiry = require('../index');
 
 describe('#Testing Suite for credit-card-expiry-validator', function() {
         it('The "anti-patterns"', function() {
-            expect(validateExpiry('4/21')).toEqual('Invalid expiry date');
-            expect(validateExpiry('1124/21')).toEqual('Invalid expiry date');
-            expect(validateExpiry('11/212')).toEqual('Invalid expiry date');
-            expect(validateExpiry('11/21222')).toEqual('Invalid expiry date');
-            expect(validateExpiry('1222/22')).toEqual('Invalid expiry date');
-            expect(validateExpiry('01/211')).toEqual('Invalid expiry date');
+            expect(creditCardValidateExpiry.isExpiryInvalid('4/21')).toEqual('Invalid expiry date');
+            expect(creditCardValidateExpiry.isExpiryInvalid('1124/21')).toEqual('Invalid expiry date');
+            expect(creditCardValidateExpiry.isExpiryInvalid('11/212')).toEqual('Invalid expiry date');
+            expect(creditCardValidateExpiry.isExpiryInvalid('11/21222')).toEqual('Invalid expiry date');
+            expect(creditCardValidateExpiry.isExpiryInvalid('1222/22')).toEqual('Invalid expiry date');
+            expect(creditCardValidateExpiry.isExpiryInvalid('01/211')).toEqual('Invalid expiry date');
         });
 
         it('Something totally irrelevant', function() {
-            expect(validateExpiry('I am just a test')).toEqual('Invalid expiry date');
+            expect(creditCardValidateExpiry.isExpiryInvalid('I am just a test')).toEqual('Invalid expiry date');
         });
 
         it('Inappropriate expiry month', function() {
-            expect(validateExpiry('14/21')).toEqual('Month should be between 01 and 12');
+            expect(creditCardValidateExpiry.isExpiryInvalid('14/21')).toEqual('Month should be between 01 and 12');
         });
 
         it('Inappropiate expiry year', function(){
-            expect(validateExpiry('11/17')).toEqual('Year is less than current year.');
-            expect(validateExpiry('11/2017')).toEqual('Year is less than current year.');
-            expect(validateExpiry('01/2011')).toEqual('Year is less than current year.');
+            expect(creditCardValidateExpiry.isExpiryInvalid('11/17')).toEqual('Year is less than current year.');
+            expect(creditCardValidateExpiry.isExpiryInvalid('11/2017')).toEqual('Year is less than current year.');
+            expect(creditCardValidateExpiry.isExpiryInvalid('01/2011')).toEqual('Year is less than current year.');
         });
 
         it('The happy-flows', function() {
-            expect(validateExpiry('01/2019')).toBeTruthy();
-            expect(validateExpiry('11/19')).toBeTruthy();
-            expect(validateExpiry('11/2019')).toBeTruthy();
-            expect(validateExpiry('12/21')).toBeTruthy();
+            expect(creditCardValidateExpiry.isExpiryInvalid('01/2019')).toBeFalsy();
+            expect(creditCardValidateExpiry.isExpiryInvalid('11/19')).toBeFalsy();
+            expect(creditCardValidateExpiry.isExpiryInvalid('11/2019')).toBeFalsy();
+            expect(creditCardValidateExpiry.isExpiryInvalid('12/21')).toBeFalsy();
         })
 });
